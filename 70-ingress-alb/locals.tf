3a11a451f7c1b19fd12eb944d506edb1d51298ff
gopih <gopi47590@gmail.com>
2026-03-27 10:32:01 +0530

    cd for backend

--- 70-ingress-alb/locals.tf
+++ 70-ingress-alb/locals.tf
@@ -2,4 +2,6 @@ locals {
   public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_id.value)
   web_alb_sg_id = data.aws_ssm_parameter.web_alb_sg_id.value
   web_lb_certificate_arn = data.aws_ssm_parameter.web_lb_certificate_arn.value
+  resource_name = "${var.project_name}-${var.environment}"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
 }
